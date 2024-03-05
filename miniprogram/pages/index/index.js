e00d079adfd4fa4808b4ae8829422d017cd1998f
岩 刘 <524643652@qq.com>
2024-03-05 10:06:45 +0800

    向网站请求数据

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -26,8 +26,22 @@ Page({
     console.log('搜索词汇：', e.detail.value);
     const searchText = this.data.searchText;
     console.log('搜索词汇：', searchText);
-    wx.navigateTo({
-      url: '/pages/searchResult/searchResult?searchText=' + searchText
+    // wx.navigateTo({
+    //   url: '/pages/searchResult/searchResult?searchText=' + searchText
+    // });
+
+    wx.request({
+      url: 'https://qwerty.kaiyi.cool/dicts/itVocabulary.json', // 请求的网址
+      method: 'GET', // 请求方法
+      success: function(res) {
+        console.log('请求成功', res.data); // 打印返回的数据
+        // 在这里处理返回的数据，将其展示到前台
+      },
+      fail: function(error) {
+        console.error('请求失败', error);
+        // 处理请求失败的情况
+      }
     });
+    
   }
 })
