90606750227ab65f9d611b8f7befb62c9c2ecccf
岩 刘 <524643652@qq.com>
2024-03-05 11:44:29 +0800

    搜索成功并修改结果展示

--- cloudfunctions/search/index.js
+++ cloudfunctions/search/index.js
@@ -6,18 +6,35 @@ cloud.init({
 
 const db = cloud.database()
 
-exports.main = async (event, context) => {
+exports.main = async (event) => {
   try {
+    const wordName = event.searchQuery; // 获取传入的单词名称
+    console.log('传入的单词:', wordName);
+    // 确保传入了单词名称
+    if (!wordName) {
+      return {
+        code: 400,
+        message: '未提供单词名称'
+      };
+    }
+
     // 执行简单的数据库查询
-    const result = await db.collection('words').where({
-      word: 'command'
+    const result = await db.collection('ITVocabulary').where({
+      name: wordName
     }).get();
 
-    // 返回查询结果
-    return {
-      code: 200,
-      data: result.data,
-      message: '查询成功'
+    // 如果找到匹配的单词，则返回结果
+    if (result.data.length > 0) {
+      return {
+        code: 200,
+        data: result.data,
+        message: '查询成功'
+      };
+    } else {
+      return {
+        code: 404,
+        message: '未找到匹配的单词'
+      };
     }
   } catch (e) {
     return {
--- miniprogram/pages/searchResult/searchResult.js
+++ miniprogram/pages/searchResult/searchResult.js
@@ -29,23 +29,42 @@ Page({
         },
         success: function(res) {
           console.log('搜索结果:', res.result);
+          // 检查结果是否包含有效数据
+          // if (res.result && res.result.data && Array.isArray(res.result.data)) {
+          //   that.setData({
+          //     searchResults: res.result.data.map((item) => {
+          //       return {
+          //         name: item.name,
+          //         trans: item.trans,
+          //         usphone: item.usphone,
+          //         ukphone: item.ukphone,
+          //       }
+          //     })
+          //   });
           // 处理搜索结果
-        that.setData({
-          searchResults: res.result.data.map((item) => {
-            return {
-              word: item.word,
-              type: item.type,
-              translation: item.translation,
-              pronounce: item.pronounce,
-            }
-          })
-        });
+          if (res.result.code === 200) {
+            let formattedResults = res.result.data.map((item) => {
+              return {
+                name: item.name,
+                trans: item.trans,
+                usphone: item.usphone,
+                ukphone: item.ukphone
+              }
+            });
+
+            that.setData({
+              searchResults: formattedResults
+            });
+          } else {
+            console.error('查询失败:', res.result.message);
+          }
+          // 在成功回调函数内部输出日志
+          console.log('调用云函数完毕');
         },
         fail: function(error) {
           console.error(error)
         }
-      }),
-      console.log('调用云函数完毕');
+      })
     }
   }
 })
