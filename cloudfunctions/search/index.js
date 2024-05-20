0d863cd513d555a38ae511d4e2a8adb1eba58b27
岩 刘 <524643652@qq.com>
2024-05-20 21:49:56 +0800

    增加csharp、sql和go三个数据库

--- cloudfunctions/search/index.js
+++ cloudfunctions/search/index.js
@@ -6,6 +6,31 @@ cloud.init({
 
 const db = cloud.database()
 
+// 查询数据库的函数
+async function queryDatabase(collectionName, wordName) {
+  let result = await db.collection(collectionName).where({
+    $or: [
+      {
+        name: {
+          $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
+          $options: 'i' // 忽略大小写
+        }
+      },
+      {
+        "trans.0": {
+          $regex: `.*${wordName}.*`,
+          $options: 'i'
+        }
+      }
+    ]
+  }).get();
+  // 添加来源信息
+  result.data.forEach(item => {
+    item.source = collectionName; // 使用数据库集合名称作为来源信息
+  });
+  return result.data;
+}
+
 exports.main = async (event) => {
   try {
     const wordName = event.searchQuery; // 获取传入的单词名称
@@ -21,126 +46,12 @@ exports.main = async (event) => {
     // 定义一个空数组来存储所有结果
     const results = [];
 
-    // 在第一个数据库中查询
-    let result = await db.collection('ITVocabulary').where({
-      $or: [
-        {
-          name: {
-            $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
-            $options: 'i' // 忽略大小写
-          }
-        },
-        {
-          "trans.0": {
-            $regex: `.*${wordName}.*`,
-            $options: 'i'
-          }
-        }
-      ]
-    }).get();
-    // 添加来源信息
-    result.data.forEach(item => {
-      item.source = '计算机通用';
-    });
-    // 将结果添加到数组中
-    results.push(...result.data);
-
-    // 在第二个数据库中查询
-    result = await db.collection('LinuxCommand').where({
-      $or: [
-        {
-          name: {
-            $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
-            $options: 'i' // 忽略大小写
-          }
-        },
-        {
-          "trans.0": {
-            $regex: `.*${wordName}.*`,
-            $options: 'i'
-          }
-        }
-      ]
-    }).get();
-    // 添加来源信息
-    result.data.forEach(item => {
-      item.source = 'Linux命令';
-    });
-    // 将结果添加到数组中
-    results.push(...result.data);
-
-    // 在第三个数据库中查询
-    result = await db.collection('AIMachineLearning').where({
-      $or: [
-        {
-          name: {
-            $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
-            $options: 'i' // 忽略大小写
-          }
-        },
-        {
-          "trans.0": {
-            $regex: `.*${wordName}.*`,
-            $options: 'i'
-          }
-        }
-      ]
-    }).get();
-    // 添加来源信息
-    result.data.forEach(item => {
-      item.source = '机器学习';
-    });
-    // 将结果添加到数组中
-    results.push(...result.data);
-    
-    // 在第四个数据库中查询
-    result = await db.collection('AIForScience').where({
-      $or: [
-        {
-          name: {
-            $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
-            $options: 'i' // 忽略大小写
-          }
-        },
-        {
-          "trans.0": {
-            $regex: `.*${wordName}.*`,
-            $options: 'i'
-          }
-        }
-      ]
-    }).get();
-    // 添加来源信息
-    result.data.forEach(item => {
-      item.source = '人工智能';
-    });
-    // 将结果添加到数组中
-    results.push(...result.data);
-
-    // 在第五个数据库中查询
-    result = await db.collection('newwordsfromusers').where({
-      $or: [
-        {
-          name: {
-            $regex: `.*${wordName}.*`, // 使用正则表达式进行模糊匹配
-            $options: 'i' // 忽略大小写
-          }
-        },
-        {
-          "trans.0": {
-            $regex: `.*${wordName}.*`,
-            $options: 'i'
-          }
-        }
-      ]
-    }).get();
-    // 添加来源信息
-    result.data.forEach(item => {
-      item.source = '用户上传';
-    });
-    // 将结果添加到数组中
-    results.push(...result.data);
-
+    // 查询多个数据库
+    const collections = ['ITVocabulary', 'LinuxCommand', 'AIMachineLearning', 'AIForScience', 'newwordsfromusers', 'csharp','SQL_statement', 'go'];
+    for (let collection of collections) {
+      const collectionResults = await queryDatabase(collection, wordName);
+      results.push(...collectionResults);
+    }
 
     // 如果找到匹配的单词，则返回结果
     if (results.length > 0) {
@@ -156,7 +67,7 @@ exports.main = async (event) => {
       code: 404,
       message: '未找到匹配的单词'
     };
-    } catch (e) {
+  } catch (e) {
     return {
       code: 500,
       message: `查询失败: ${e}`
